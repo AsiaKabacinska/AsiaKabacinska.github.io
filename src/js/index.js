0f7dda2561747993c80e186ad328c52a8fa819bc
Asia Kabacińska <joannakabatek@gmail.com>
2021-08-16 13:28:49 +0200

    fix for github icon,js loading for repositories,links in new tab,hand animation

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import "../scss/main.scss";
 
-console.log("HELLO 🚀");
+console.log("Hi, I'm Asia - nice to meet you  🚀");
 
 fetch(
   "https://api.github.com/users/asiakabacinska/repos?sort=created&direction=asc"
@@ -10,47 +10,51 @@ fetch(
     const container = document.querySelector(".projects-grid--js");
     for (let repo of res) {
       const { description, homepage, html_url, name } = repo;
-      //   console.log(repo);
       const template = ` <article class="project">
-      <div class="project__window">
-        <span class="project__circle"></span
-        <span class="project__circle"></span>
-        <span class="project__circle"></span>
-      </div>
-      <div class="project__content">
-        <img src="../assets/img/github-projects.svg" alt="" />
-        <h3 class="project__grid project__title">
-          <span class="project__label">project:</span>
-          <span>${name}</span>
-        </h3>
-        <p class="project__grid project__grid--description">
-          <span class="project__label">description:</span>
-          <span>${description}</span>
-        </p>
-        <p class="project__grid">
-          <span class="project__label">demo:</span>
-          <span
-            >&lt;<a
-              class="project__link"
-              href="${homepage}"
-              title="${name}"
-              >see_here</a
-            >&gt;</span
-          >
-        </p>
-        <p class="project__grid">
-          <span class="project__label">github:</span>
-          <span
-            >&lt;<a
-              class="project__link"
-              href="${html_url}"
-              title="${name}"
-              >source_code</a
-            >&gt;</span
-          >
-        </p>
-      </div>
-    </article>`;
+        <div class="project__window">
+          <span class="project__circle"></span
+          <span class="project__circle"></span>
+          <span class="project__circle"></span>
+        </div>
+        <div class="project__content">
+          <img src="../assets/img/github-projects.svg" alt="" />
+          <h3 class="project__grid project__title">
+            <span class="project__label">project:</span>
+            <span>${name}</span>
+          </h3>
+          <p class="project__grid project__grid--description">
+            <span class="project__label">description:</span>
+            <span>${description}</span>
+          </p>
+          <p class="project__grid">
+            <span class="project__label">demo:</span>
+            <span
+              >&lt;<a
+              rel="noopener noreferrer"
+              target = "_blank"
+                class="project__link"
+                href="${homepage}"
+                title="${name}"
+                >see_here</a
+              >&gt;</span
+            >
+          </p>
+          <p class="project__grid">
+            <span class="project__label">github:</span>
+            <span
+              >&lt;<a
+                class="project__link"
+                href="${html_url}"
+                rel="noopener noreferrer"
+                target = "_blank"
+                title="${name}"
+                >source_code</a
+              >&gt;</span
+            >
+          </p>
+        </div>
+      </article>`;
+
       if (description) {
         container.innerHTML += template;
       }
